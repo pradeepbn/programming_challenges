ca0d77c8c1d4fceec6f3c4943db597eec3e541fc
Pradeep <pradipbn@gmail.com>
2014-06-11 23:55:35 -0700

    Adding support for the array based stack

--- stack.c
+++ stack.c
@@ -4,44 +4,137 @@
 
 #include "stack.h"
 #include <stdlib.h>
+ stack_api;
 
-void new_stack(stack **s)
+#ifdef LINKED_LIST_STACK
+int linked_list_new_stack(stack **s)
 {
 	*s = (stack *)malloc(sizeof(stack));
+    if (*s == NULL) {
+        return ENOMEM;
+    }
 	(*s)->stack_ptr = NULL;
 	(*s)->stack_size = 0;
 }
 
-item_type pop(stack *s)
+int linked_list_push(stack *s, item_type item)
+{
+	NODE *current_stack_ptr;
+    void *stk_ptr;
+
+	current_stack_ptr = s->stack_ptr;
+	stk_ptr = malloc(sizeof(NODE));
+    if (stk_ptr == NULL) {
+        return ENOMEM;
+    }
+	s->stack_ptr->next = current_stack_ptr;
+	s->stack_ptr->item = item;
+	s->stack_size++;
+    return 0;
+}
+
+int linked_list_pop(stack *s, item_type *item)
 {
 	item_type ret_item;
 	NODE *current_stack_ptr;
 
 	if (s == NULL) {
-		return -1;
+		return EINVAL;
 	}
 
 	if (s->stack_size == 0) {
-		return -1;
+		return EINVAL;
 	}
-	ret_item = s->stack_ptr->item;
+	*item = s->stack_ptr->item;
 	current_stack_ptr = s->stack_ptr;
 	s->stack_ptr = s->stack_ptr->next;
 	s->stack_size -= 1;
 	free(current_stack_ptr);
-	return ret_item;
+	return 0;
 }
+#endif
 
-void push(stack *s, item_type item)
+#ifdef ARRAY_STACK
+int linked_list_new_stack(stack **s)
 {
-	NODE *current_stack_ptr;
-
-	current_stack_ptr = s->stack_ptr;
-	s->stack_ptr = (NODE *) malloc(sizeof(NODE));
-	s->stack_ptr->next = current_stack_ptr;
-	s->stack_ptr->item = item;
-	s->stack_size++;
+	*s = (stack *)malloc(sizeof(stack));
+    if (*s == NULL) {
+        return ENOMEM;
+    }
+    (*s)->allocated_ptr = malloc(DEFAULT_ARRAY_STACK_SIZE);
+    if ((*s)->allocated_ptr) {
+        return ENOMEM;
+    }
+	(*s)->stack_ptr = (item_tyoe *)(*s)->allocated_ptr;
+	(*s)->stack_size = 0;
+    (*s)->allocated_size_bytes = DEFAULT_ARRAY_STACK_SIZE;
+    return 0;
 }
+
+int array_push(stack *s, item_type item)
+{
+    unsigned int stack_size_bytes;
+    void *stk_ptr;
+
+    /*
+     * Reallocate the STACK array if the size of stack grows
+     * beyond the allocated size
+     */
+    stack_size_bytes = (s->stack_size * sizeof(item_type));
+    if ((stack_size_bytes + sizeof(item_type)) > s->allocated_size) {
+        stk_ptr = realloc(s->stack_ptr, stack_size_bytes << 1);
+        if (stk_ptr == NULL) {
+            return ENOMEM;
+        } else {
+            s->allocated_ptr = stk_ptr;
+            s->allocated_size_bytes = stack_size_bytes;
+            s->stack_ptr = (item_type *)stk_ptr + s->stack_size;
+        }
+    }
+
+    /*
+     * Executing the PUSH operation
+     */
+    (s->stack_ptr)++;
+    memcpy(s->stack_ptr, item, sizeof(item_type));
+    s->stack_size++;
+    return 0;
+}
+
+int array_pop(stack *s, item_type *item)
+{
+    unsigned int allocated_size_bytes;
+
+	if (s == NULL) {
+		return EINVAL;
+	}
+
+	if (s->stack_size == 0) {
+		return EINVAL;
+	}
+
+    *item = *s->stack_ptr;
+    s->stack_size -= 1;
+    s->stack_ptr -= 1;
+
+    /*
+     * Reduce the size of the stack
+     * if the stack occupancy is less than half
+     */
+    allocated_size_bytes = s->allocated_size_bytes;
+    if ((s->stack_size * sizeof(item_type)) <
+            allocated_size_bytes >> 1) {
+        stk_ptr = realloc(s->allocated_ptr, allocated_size >> 1);
+        if (stk_ptr != NULL) {
+            s->allocated_ptr = stk_ptr;
+            s->stack_ptr = (item_type *)stk_ptr + s->stack_size;
+        }
+    }
+
+    return 0;
+}
+
+#endif
 
 bool empty(const stack *s)
 {
@@ -56,3 +149,18 @@ unsigned int size(const stack *s)
 {
 	return s->stack_size;
 }
+
+STACK_API stack_api = {
+#ifdef LINKED_LIST_STACK
+    _new_stack = linked_list_new_stack,
+    _push      = linked_list_push,
+    _pop       = linked_list_pop,
+#else
+    _new_stack = array_stack,
+    _push      = array_push,
+    _pop       = array_pop,
+#endif
+    _empty     = empty,
+    _size      = size,
+};
+
--- stack.h
+++ stack.h
@@ -3,6 +3,10 @@
  */
 #include <stdbool.h>
 
+#define DEFAULT_ARRAY_STACK_SIZE 4096
+#define ARRAY_STACK
+#undef LINKED_LIST_STACK
+
 typedef int item_type;
 
 typedef struct node {
@@ -10,13 +14,46 @@ typedef struct node {
 	struct node *next;
 } NODE;
 
+#ifdef LINKED_LIST_STACK
 typedef struct stack {
 	NODE *stack_ptr;
 	unsigned int stack_size;
 } stack;
+#endif
 
-void new_stack(stack **s);
-item_type pop(stack *s);
-void push(stack *s, item_type item);
-bool empty(const stack *s);
-unsigned int size(const stack *s);
+#ifdef ARRAY_STACK
+typedef struct stack {
+    void *allocated_ptr;
+    item_type *stack_ptr;
+    /* Number of item_types */
+    unsigned int stack_size;
+    unsigned int allocated_size_bytes;
+} stack;
+#endif
+
+typedef stack_api {
+    (int) (*_new_stack(stack **s));
+    (item_type) (*_pop(stack *s));
+    (int) (*_push(stack *s, item_type item));
+    (bool) (*_empty(const stack *s));
+    (unsigned int) (*_size(const stack *s));
+} STACK_API;
+
+extern STACK_API stack_api;
+
+#define new_stack(n_stack_ptr) \
+    stack_api._new_stack(n_stack_ptr);
+
+#define push(stack_ptr, element) \
+    stack_api._push(stack_ptr, element);
+
+#define pop(stack_ptr) \
+    stack_api._pop(stack_ptr);
+
+#define empty(stack_ptr) \
+    stack_api._empty(stack_ptr);
+
+#define size(stack_ptr) \
+    stack_api._size(stack_ptr);
+
+
