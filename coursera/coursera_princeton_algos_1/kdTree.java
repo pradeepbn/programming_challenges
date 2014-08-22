d2f3da4be9ce3169007aaeb1cb9b0cb1d2c198e8
pradipbn@gmail.com <pradipbn@gmail.com>
2014-08-22 09:43:39 -0700

    kdTree: insert, contains implementation

--- coursera/coursera_princeton_algos_1/kdTree.java
+++ coursera/coursera_princeton_algos_1/kdTree.java
@@ -7,8 +7,8 @@ public class kdTree {
 	   private Node lb;        // the left/bottom subtree
 	   private Node rt;        // the right/top subtree
 
-	   public Node() {
-		   p = null;
+	   public Node(point2D point) {
+		   p = point;
 		   rect = null;
 		   lb = null;
 		   rt = null;
@@ -29,23 +29,57 @@ public class kdTree {
 	   }
 	}
 	public int size() {
-	// number of points in the set
+		// number of points in the set
+		return size;
 	}
 	public void insert(Point2D p) {
-	// add the point p to the set (if it is not already in the set)
+		// add the point p to the set (if it is not already in the set)
+		// 1 - vertical orientation; 0 - horizontal orientation
 		Node node;
-		if (kdNode == null) {
-			kdNode = new kdNode();
-			kdNode.p = p;
-			node = kdNode;
-		}
+		kdNode = insert(kdNode, p, 0);
+	}
+	/* 
+	 * Orientation refers to the parent orientation
+	 * Every child should reverse orientation passed by the parent
+	 */
+	private Node insert(Node node, point2D point, int orientation) {
 
-		if (splitAxis == 0) {
-			node.rect = new rect();
+		if (node == null) {
+		   	node = new Node(point);
+			size++;
+			//create a rect using the orientation
+			if (orientation == 0) {
+			} else {
+			}
+
+			return node;
 		}
+		int cmp = node.p.compare(point);
+
+		if (cmp == -1) {
+			if (orientation == 0) { orientation = 1; }
+			else { orientation = 0; }
+		   	node.lb = insert(node.lb, point, orientation);
+		} else if (cmp == 1) {
+			if (orientation == 0) { orientation = 1; }
+			else { orientation = 0; }
+		   	node.rt = insert(node.rt, point, orientation);
+	   	}
+		return node;
 	}
 	public boolean contains(Point2D p) {
 	// does the set contain the point p?
+		Node node = kdNode;
+		while (node != null) {
+			if (node.p.compare(p) == 0) {
+				return true;
+			} else if (node.p.compare(p) == -1) {
+				node = kdNode.lb;
+			} else if (node.p.compare(p) == 1) {
+				node = kdNode.rt;
+			}
+			return false;
+		}
 	}
 	public void draw() {
 	// draw all of the points to standard draw
