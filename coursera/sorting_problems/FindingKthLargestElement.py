06648404649741614b0ab2cedf0055e367735c51
pradipbn@gmail.com <pradipbn@gmail.com>
2014-09-28 20:36:40 -0700

    Algorithm: Finding the kth largest element (V1.0)

--- coursera/sorting_problems/FindingKthLargestElement.py
+++ coursera/sorting_problems/FindingKthLargestElement.py
@@ -1,27 +1,64 @@
-def binarySearchReplaceMedian(k, len_a, len_b, mergedArray):
-    if (len_b == 0):
-        #Get this working tomorrow
-def findKthLargestElement(k, len_a, len_b, mergedArray): 
-    #a[0] = 30;  
-    if (mergedArray[len_a - 1] > mergedArray[k - 1]):
-        binarySearchReplaceMedian(k, len_a, 0, mergedArray);
-    else:
+even = lambda lo, hi: (lo + hi) % 2;
+mid_even = lambda lo, hi: ((lo + hi) / 2);
+mid_odd = lambda lo, hi: ((lo + hi) / 2);
+
+def binarySearchReplaceMedian(k, lo, hi, mergedArray):
+    print lo, hi, mergedArray[k - 1];
+    print mergedArray;
+    if (lo + 1 == hi):
+        print "swapping"
+        temp = mergedArray[k - 1]
+        mergedArray[k - 1] = mergedArray[lo + 1];
+        mergedArray[lo + 1] = temp;
+        print mergedArray;
         return;
 
-    if (mergedArray[k] < mergedArray[k - 1]):
-        binarySearchReplaceMedian(k, 0, len_b, mergedArray);
+    if (even(lo, hi) == 0):
+        mid = mid_even(lo, hi);
+    else:
+        mid = mid_odd(lo, hi);
+    print mid;
+    if (k - 1 == mid):
+        if (mergedArray[k - 1] < mergedArray[mid - 1]):
+            temp = mergedArray[k - 1];
+            mergedArray[k - 1] = mergedArray[mid - 1];
+            mergedArray[mid - 1] = temp;
+            return;
+        elif (mergedArray[k - 1] > mergedArray[mid + 1]) or (k - 1 == mid):
+            temp = mergedArray[k - 1];
+            mergedArray[k - 1] = mergedArray[mid + 1];
+            mergedArray[mid + 1] = temp;
+            return;
+
+    if (mergedArray[k - 1] < mergedArray[mid]):
+        binarySearchReplaceMedian(k, lo, mid, mergedArray);
+    elif (mergedArray[k - 1] > mergedArray[mid]):
+        binarySearchReplaceMedian(k, mid, hi, mergedArray);
+
+def findKthLargestElement(k, len_a, len_b, mergedArray): 
+    #a[0] = 30;  
+    print "Finding";
+    if (mergedArray[len_a - 1] > mergedArray[k - 1]):
+        print "In first block"
+        binarySearchReplaceMedian(k, 0, len_a - 1, mergedArray);
+    elif (mergedArray[k] < mergedArray[k - 1]):
+        print "In Second block"
+        binarySearchReplaceMedian(k, len_a, (len_a + len_b - 1), mergedArray);
+    else:
+        return;
 
     findKthLargestElement(k, len_a, len_b, mergedArray);
     print (k);
 
-
 def main():
-    a = [1, 4, 7, 10];
-    b = [2, 3, 5, 6, 8, 9];
+    a = [1, 4, 10, 78, 100];
+    b = [2, 3, 70, 75, 80];
     k = 3;
     c = a + b;
-    findKthLargestElement(len(c) - k, len(a), len(b), c);
-    print c;
+    print even(0, 5);
+    #print mid_odd(0, 5);
+    findKthLargestElement(len(c) - k + 1, len(a), len(b), c);
+    print k, "the largest element is:", c[len(c) - k];
 
 if __name__ == "__main__":
     main()
