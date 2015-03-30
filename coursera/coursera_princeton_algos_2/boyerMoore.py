bc56bc78478327697a59eb8dda49648528cf529c
pradeepbn <pradipbn@gmail.com>
2015-03-30 09:38:28 -0700

    Substring matching using Boyer Moore algorithm

---
+++ coursera/coursera_princeton_algos_2/boyerMoore.py
@@ -0,0 +1,41 @@
+R = 26;
+rMatrix = [];
+charToInt = lambda char : (ord(char) - ord('a'));
+
+def computeRightMostCharacterMatrix(pattern):
+    for i in range(0, 26):
+        rMatrix.append(-1);
+    for j in range(0, len(pattern)):
+        rMatrix[charToInt(pattern[j])] = j; 
+
+def matchSubstring(text, pattern):
+    computeRightMostCharacterMatrix(pattern);
+    print rMatrix;
+    M = len(pattern);
+    skip = 0;
+    i = 0;
+    while i < len(text):
+        for j in range(M - 1, -1, -1):
+            if (text[i + j] != pattern[j]):
+                if (rMatrix[charToInt(text[i + j])] == -1):
+                    '''
+                        Case 1: Mismatch character not found in the pattern 
+                    '''
+                    skip = j;
+                else:
+                    skip = max(1, j - rMatrix[charToInt(text[i + j])]);
+
+                i = i + skip; 
+                break;
+            if (j == 0):
+                return i;
+
+
+if __name__ == '__main__':
+    pattern = "ababac"
+    text = "mnopqrstababactuvew";
+#print text[matchSubstring(text, pattern)];
+#    print rMatrix;
+    import timeit
+    print(timeit.timeit("matchSubstring(text, pattern)"));#, setup="from __main__ "))
+
