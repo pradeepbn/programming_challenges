beceaab72cd95445677011f1a0a08ec55b80173c
pradipbn@gmail.com <pradipbn@gmail.com>
2015-04-07 09:48:25 -0700

    Anagrams: Trying nested multimap

--- c_plus_plus_training/anagrams.cpp
+++ c_plus_plus_training/anagrams.cpp
@@ -8,28 +8,43 @@ using namespace std;
 vector<string>
 anagrams(vector<string> &strs) {
     vector<string> result;
-    unordered_multimap<string, string> anagramHash;
+    unordered_multimap<string, unordered_multimap<string, string>> anagramHash;
     string sLocal;
 
     for (auto& str: strs) {
         sLocal.assign(str);
         sort(sLocal.begin(), sLocal.end());
-        cout << sLocal << endl;
-        anagramHash.emplace(sLocal, str);
+        //cout << sLocal << endl;
+		auto it = anagramHash.find(sLocal);
+		if (it != unordered_multimap::end) {
+			(it->second).insert(make_pair(sLocal, str));
+		} else {
+			anagramHash.emplace(sLocal, make_pair(sLocal, str));
+		} 
+			
     }
 
-    cout << anagramHash.bucket_count() << endl;
-    for (unsigned i = 0; i < anagramHash.bucket_count(); i++) {
-        if (anagramHash.bucket_size(i) > 1) {
-            cout << anagramHash.bucket_size(i) << endl;
-            for (auto it = anagramHash.begin(i); it != anagramHash.end(i); ++it) {
-                cout << anagramHash.hash_function()(it->first) << ";";
-                cout << it->second << endl;
-                result.push_back(it->second);
-            }
-            cout << endl;
-        }
-    }
+    //cout << anagramHash.bucket_count() << endl;
+    //innerItfor (unsigned i = 0; i < anagramHash.bucket_count(); i++) {
+    //innerIt    if (anagramHash.bucket_size(i) > 1) {
+    //innerIt        cout << anagramHash.bucket_size(i) << endl;
+    //innerIt        for (auto it = anagramHash.begin(i); it != anagramHash.end(i); ++it) {
+	//innerIt			for (j = 0; j < (it->second).bucket_count(); j++) {
+	//innerIt				if ((it->second).bucket_size(j) > 1) {
+	//innerIt					for (auto innerIt = (it->second).begin(j); 
+	//innerIt							innerIt != (it->second).end(j); innerIt) {
+	//innerIt						result.push_back(innerIt->second);
+	//innerIt					}
+	//innerIt				}
+	//innerIt			}
+	//innerIt			}
+    //innerIt            //cout << anagramHash.hash_function()(it->first) << ";";
+    //innerIt            //cout << it->second << endl;
+    //innerIt            //result.push_back(it->second);
+    //innerIt        }
+    //innerIt        cout << endl;
+    //innerIt    }
+    //innerIt}
 
     return result;
 }
