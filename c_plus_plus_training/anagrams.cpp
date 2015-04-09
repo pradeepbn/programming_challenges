536fa577688923c94f35113da74054af8dc1c6b0
pradipbn@gmail.com <pradipbn@gmail.com>
2015-04-09 10:19:22 -0700

    Optimizations: bucket sort implementation for character sorting. Test cases updated on anagrams.txt

--- c_plus_plus_training/anagrams.cpp
+++ c_plus_plus_training/anagrams.cpp
@@ -2,53 +2,36 @@
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
+#include <cstring>
 
 using namespace std;
-//
-//vector<string>
-//anagrams(vector<string> &strs) {
-//    //vector<string> result;
-//    unordered_multimap<string, vector<string>> anagramHash;
-//    string sLocal;
-//
-//	anagramHash.reserve(20);
-//
-//	//cout << strs.size() << endl;
-//	if (strs.size() == 1) {
-//		//result.push_back(strs.pop_back());
-//		return strs;
-//	}
-//
-//    for (string str: strs) {
-//        sLocal.assign(str);
-//        sort(sLocal.begin(), sLocal.end());
-//		//cout << "sLocal:" << sLocal << endl;
-//		auto it = anagramHash.find(sLocal);
-//		if (it != anagramHash.end()) {
-//			(it->second).push_back(str);
-//		} else {
-//			vector<string> lVector;
-//			lVector.push_back(str);
-//			anagramHash.emplace(sLocal, lVector);
-//		} 
-//			
-//    }
-//
-//    for (unsigned i = 0; i < anagramHash.bucket_count(); i++) {
-//		//cout << "Bucket size:" << anagramHash.bucket_size(i) << endl;
-//        if (anagramHash.bucket_size(i) > 0) {
-//			for (auto it = anagramHash.begin(i); it != anagramHash.end(i); ++it) {
-//				//cout << "Item size:" << (it->second).size() << endl;
-//				if ((it->second).size() > 1) {
-//					return it->second;
-//					/*for (auto str : (it->second)) {
-//						result.push_back(str);
-//					}*/
-//				}
-//            }
-//        }
-//    }
-//}*/
+
+#define RADIX 256
+
+
+char* bucketSort(string str) {
+	int rCount[RADIX + 1] = {0};
+	int length = str.length();
+
+	char *aux = (char *)malloc(length + 1);
+
+	for (auto &chr : str) {
+		rCount[((int) chr) + 1]++;
+	}
+
+	for (int i = 0; i < RADIX; i++) {
+		rCount[i + 1] += rCount[i];
+	}
+
+	for (auto &chr : str) {
+		aux[rCount[chr]++] = chr;
+	}
+
+	aux[length] = '\0';
+
+	return aux;
+}
+
 vector<string>
 anagrams(vector<string> &strs) {
 	unordered_multimap<string, vector<string>> anagramHash;
@@ -58,15 +41,19 @@ anagrams(vector<string> &strs) {
 	anagramHash.reserve(20);
 	
 	if (strs.size() == 1) {
-		if (strs.back().compare("") == 0) {
-			strs.pop_back();
-		}
 		return strs;
 	}
 
 	for (string str: strs) {
-		sLocal.assign(str);
-		sort(sLocal.begin(), sLocal.end());
+		//sLocal.assign(str);
+		//sort(sLocal.begin(), sLocal.end());
+		string sLocal;
+		if (str.length() > 1) {
+			sLocal = string(bucketSort(str));
+		} else {
+			sLocal.assign(str);
+		}
+
 		auto it = anagramHash.find(sLocal);
 		if (it != anagramHash.end()) {
 			if ((it->second).size() == 1) {
